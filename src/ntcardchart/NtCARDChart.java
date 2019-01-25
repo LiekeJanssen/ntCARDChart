2f1f0fcc1cbdf545cc280033737ee4fb26cd3b5d
ljanssen <ljanssen@ljanssen01.phage.bcgsc.ca>
2019-01-25 11:29:25 -0800

    Final commit

--- src/ntcardchart/NtCARDChart.java
+++ src/ntcardchart/NtCARDChart.java
@@ -27,43 +27,44 @@ public class NtCARDChart {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        JFrame frame = new JFrame();
-        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        JFrame frame = new JFrame();                //create JFrame
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();      //set dataset
         //String path = args[0];
         
-        String path1 = "/home/ljanssen/Documents/stuff/ERR_k20.hist";
-        String path2 = "/home/ljanssen/Documents/stuff/ERR.hist";
+        String path1 = "/home/ljanssen/Documents/SPAdes_sample_data/testntcard_k100.hist";  //path to the file
+        String path2 = "/home/ljanssen/Stuff/ERR.hist"; //(args[0]);                        //path to file, in commented part rather use args[0] for user input
         
-        try (BufferedReader br = new BufferedReader(new FileReader(path2))){
-            String line = null;
-            line = br.readLine();
-            if (line.contains("F1")){
+        try (BufferedReader br = new BufferedReader(new FileReader(path2))){            //read file from path2
+            String line = null;                                                         //make variable line empty
+            line = br.readLine();                                                       //read first line
+            if (line.contains("F1")){                                                   
                 line = br.readLine();
-                String[] cols = null;
-                while ((line = br.readLine()) != null){
-                    String[] tmp = path1.split("/");
+                String[] cols = null;                                                   //creating an array "cols" when the column F1 is found in the file and 
+                while ((line = br.readLine()) != null){                                 //read the lines
+                    String[] tmp = path1.split("/");                                    //using the file names to put on the image later
                     String series = tmp[tmp.length-1];
                     cols = line.split("\t");
-                    String uniqueK1 = cols[0];
+                    String uniqueK1 = cols[0];                                          
 
                     String numbK1 = cols[1];
                     Integer x = Integer.parseInt(uniqueK1); 
                     Integer y = Integer.parseInt(numbK1);
 
-                    dataset.addValue(y, series, x);
+                    dataset.addValue(y, series, x);                                     //column 1 is the unique kmer column and numbK1 is the column with the coverage, put those on the x- and y-axis
 
                 }
 
-                JFreeChart chart = ChartFactory.createLineChart("K-mer Coverage Histogram", "Kmer coverage", "Frequency", dataset, PlotOrientation.VERTICAL, true, true, false);
+                JFreeChart chart = ChartFactory.createLineChart("K-mer Coverage Histogram", "Kmer coverage", "Frequency", dataset, PlotOrientation.VERTICAL, true, true, false);        //create and put data in JFreeChart
 
 
                 ChartPanel chartPanel = new ChartPanel(chart);
                 frame.add(chartPanel);
                 frame.setMinimumSize(new Dimension(800,800));
                 frame.setVisible(true);
+                                                                                            //set sizes and make visible
             }
             else {
-                String[] cols2 = null;
+                String[] cols2 = null;                                                        //when column does not start with F1 go on with the same
                 while ((line = br.readLine()) != null){
                     cols2 = line.split("\t");
                     String series = cols2[0];
@@ -74,35 +75,6 @@ public class NtCARDChart {
                     
                     dataset.addValue(y, series, x);
                     
-                    /*
-                    if (Kmer.equals("20")){
-                        String series1 = "K"+cols2[0];
-                        String y20 = cols2[1];
-                        Integer Y20 = Integer.parseInt(y20);
-                        String x20 = cols2[2];
-                        Integer X20 = Integer.parseInt(x20);
-
-                        dataset.addValue(X20, series1, Y20);
-                    }
-                    else if (Kmer.equals("30")){
-                        String series2 = "K"+cols2[0];
-                        String y30 = cols2[1];
-                        Integer Y30 = Integer.parseInt(y30);
-                        String x30 = cols2[2];
-                        Integer X30 = Integer.parseInt(x30);
-
-                        dataset.addValue(X30, series2, Y30);
-                    }
-                    else if (Kmer.equals("40")){
-                        String series3 = "K"+cols2[0];
-                        String y40 = cols2[1];
-                        Integer Y40 = Integer.parseInt(y40);
-                        String x40 = cols2[2];
-                        Integer X40 = Integer.parseInt(x40);
-
-                        dataset.addValue(X40, series3, Y40);
-                    }
-                    */
                 }
 
                 JFreeChart chart = ChartFactory.createLineChart("K-mer Coverage Histogram", "Kmer coverage", "Frequency", dataset, PlotOrientation.VERTICAL, true, true, false);
